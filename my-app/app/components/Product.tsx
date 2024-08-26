bd9f7cf91d8452a5c103074e70ebe2410433ccbe
kbotic00 <kbotic00@fesb.hr>
2024-08-26 22:21:10 +0200

    poboljsanm home za male ekrane

--- my-app/app/components/Product.tsx
+++ my-app/app/components/Product.tsx
@@ -10,7 +10,7 @@ const Product = () => {
         <>
         <div className=" w-[130px] lg:w-[170px]  flex gap-x-8 gap-y-16 justify-between flex-wrap relative rounded-3xl shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
 
-        <Link href="/test" className=" full flex flex-col w-[20%] sm:w-[45%] lg:w-[22%]">
+        <Link href="/test" className=" full flex flex-col  sm:w-[45%] lg:w-[22%]">
             <div>
                 {/*slika  */}
                 <div className=" relative  w-[130px] lg:w-[170px]  h-[163px] ">
--- my-app/app/components/Productlist.tsx
+++ my-app/app/components/Productlist.tsx
@@ -4,14 +4,20 @@ import Product from "./Product"
 const Productlist =()=>{
  return(
     <>
-    <div className="hidden sm:grid sm:grid-cols-5 sm:py-5 sm:pl-8 sm:justify-center sm:gap-1 ">
+    <div className="hidden md:grid md:grid-cols-5 md:py-5 md:pl-8 md:justify-center md:gap-1 ">
         <Product/>
         <Product/>
         <Product/>
         <Product/>
         <Product/>
     </div>
-   
+
+    <div className="hidden sm:grid sm:grid-cols-3 sm:py-5 sm:pl-8 sm:justify-center sm:gap-1 md:hidden lg:hidden  ">
+        <Product/>
+        <Product/>
+        <Product/>
+    </div>
+
     <div className="grid grid-cols-2 justify-center items-center py-5 pl-[8%] gap-3 sm:hidden mx-auto">
         <Product />
         <Product />
