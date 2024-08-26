57fb854554447fab2218319e78c687dcd21ed121
kbotic00 <kbotic00@fesb.hr>
2024-08-26 22:24:41 +0200

    poboljsanm home za male ekrane boja

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -9,7 +9,7 @@ const HomePage = () => {
     <div>
       <Slider></Slider>
      
-      <div className="mt-12 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative bg-white">
+      <div className="mt-12 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
         <h1 className="text-2xl font-bold mb-4"> Dropping soon</h1>
         <Productlist/>
         <h1 className="text-2xl font-bold mb-4"> Najnovije</h1>
